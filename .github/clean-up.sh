25112d47d442451865fedc66e943dbef520c5568
Junichi Kato <j5ik2o@gmail.com>
2021-04-16 14:50:29 +0900

    Update .github

--- .github/clean-up.sh
+++ .github/clean-up.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 rm -rf "$HOME/.ivy2/local"                                                        || true
-find $HOME/Library/Caches/Coursier/v1        -name "ivydata-*.properties" -delete || true
 find $HOME/.ivy2/cache                       -name "ivydata-*.properties" -delete || true
 find $HOME/.cache/coursier/v1                -name "ivydata-*.properties" -delete || true
 find $HOME/.sbt                              -name "*.lock"               -delete || true
